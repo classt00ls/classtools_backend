f549e785a2a2bb586dfc95f533d86567c33f7ec3
d3im0s <marcfarresp@gmail.com>
2025-04-04 20:28:44 +0200

    Actualizar la configuración del servidor para escuchar en el puerto definido por la variable de entorno PORT o en el puerto 3000 por defecto.

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
   app.enableCors(options);
   // app.useGlobalGuards(new AuthGuard(new JwtService(), new Reflector()));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+
 }
 bootstrap();
